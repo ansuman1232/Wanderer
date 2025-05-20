bb2c2a9abf4e5ccf1f2730557d772e84aa277ed2
Ansuman Mishra <anshumanmishra1232@gmail.com>
2025-05-20 16:28:19 +0530

    image

--- models/listing.js
+++ models/listing.js
@@ -11,7 +11,10 @@ const listingSchema= new Schema({
     },
     image:{
         type:String,
-        default:'https://plus.unsplash.com/premium_photo-1676228998257-baac26ee1191?q=80&w=1954&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',//default image
+        default:'https://plus.unsplash.com/premium_photo-1676228998257-baac26ee1191?q=80&w=1954&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',//if image file is not sent from frontend 
+        // ex:- sending by postman 
+        
+        
          set:(v)=>v===""?'https://plus.unsplash.com/premium_photo-1676228998257-baac26ee1191?q=80&w=1954&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D':v ,//if image
          //is blank then paste the image else go with given image link 
     },
